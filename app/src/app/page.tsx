1a4a79dda68e46bb4785d8e77c30d88e82610632
m0kyoji <1673517+m0kyoji@users.noreply.github.com>
2023-12-05 09:20:03 +0900

    faviconの追加

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,20 +1,28 @@
 import { Header } from "@/components/header";
 import { InputForm } from "@/components/inputForm";
 import { Result } from "@/components/result";
+import Head from "next/head";
 
 export default function Home() {
-
   return (
-    <main>
-      <section>
-        <Header/>
-      </section>
-      <section>
-        <InputForm/>
-      </section>
-      <section>
-        <Result/>
-      </section>
-    </main>
+      <>
+        <Head>
+          <title>おひるね計算機</title>
+          <meta name="description" content="archive"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+          <link rel="icon" href="/favicon.ico"/>
+        </Head>
+        <main>
+          <section>
+            <Header/>
+          </section>
+          <section>
+            <InputForm/>
+          </section>
+          <section>
+            <Result/>
+          </section>
+        </main>
+      </>
   )
 }
