4a1331d7896b517d5637a68f9b73aee360155391
m0kyoji <1673517+m0kyoji@users.noreply.github.com>
2023-12-05 13:01:08 +0900

    head情報を移動

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="ja">
+      <Head>
+        <title>おひるね計算機</title>
+        <meta name="description" content="おひるね計算機"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,17 +1,10 @@
 import { Header } from "@/components/header";
 import { InputForm } from "@/components/inputForm";
 import { Result } from "@/components/result";
-import Head from "next/head";
 
 export default function Home() {
   return (
       <>
-        <Head>
-          <title>おひるね計算機</title>
-          <meta name="description" content="archive"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1"/>
-          <link rel="icon" href="/favicon.ico"/>
-        </Head>
         <main>
           <section>
             <Header/>
