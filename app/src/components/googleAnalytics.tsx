d2820c8e37412418e330b9baba52430e89fc3dcd
m0kyoji <1673517+m0kyoji@users.noreply.github.com>
2023-12-13 15:49:54 +0900

    scriptの記述変更

--- app/src/components/googleAnalytics.tsx
+++ app/src/components/googleAnalytics.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { usePathname, useSearchParams } from 'next/navigation'
-import Script from 'next/script'
 import { useEffect } from 'react'
 
 import { pageview } from '@/lib/gtag'
@@ -20,20 +19,22 @@ const GoogleAnalytics = () => {
 
   return (
       <>
-        <Script
-            strategy='lazyOnload'
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-        />
-        <Script id='gtag-init' strategy='afterInteractive'>
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_ID}', {
-            page_path: window.location.pathname,
-          });
-        `}
-        </Script>
+        { GA_ID && (
+            <>
+              <script async src={ `https://www.googletagmanager.com/gtag/js?id=${ GA_ID }` }/>
+              <script
+                  dangerouslySetInnerHTML={ {
+                    __html: `
+                   window.dataLayer = window.dataLayer || [];
+                   function gtag(){dataLayer.push(arguments);}
+                   gtag('js', new Date());
+                   gtag('config', '${ GA_ID }', {
+                     page_path: window.location.pathname,
+                   });`,
+                  } }
+              />
+            </>
+        ) }
       </>
   )
 }
